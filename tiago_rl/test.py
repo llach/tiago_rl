94e4db0c56f82becef4e0030c536bcdb6edf71c7
llach <lachluca@gmail.com>
2021-06-18 10:57:09 +0200

    update test

--- tiago_rl/test.py
+++ tiago_rl/test.py
@@ -1,38 +1,29 @@
+import time
 import pybullet as p
-from time import sleep
 import pybullet_data
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-
-p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
+# set camera to view at scene
+p.resetDebugVisualizerCamera(1.1823151111602783, 120.5228271484375, -68.42454528808594,
+                             (-0.2751278877258301, -0.15310688316822052, -0.27969369292259216))
 
 p.setGravity(0, 0, -10)
 
-planeOrn = [0, 0, 0, 1]  # p.getQuaternionFromEuler([0.3,0,0])
-planeId = p.loadURDF("plane.urdf", [0, 0, -2], planeOrn)
+# load objects
+planeId = p.loadURDF("plane.urdf", basePosition=[0.0, 0.0, -0.01])
+cylId = p.loadURDF("./assets/objects/cylinder.urdf", basePosition=[0.04, 0.02, 0.6])
+robId = p.loadURDF("./assets/objects/box.urdf", basePosition=[0.0, 0.0, 0.27])
 
-boxId = p.loadURDF("cube.urdf", [0, 3, 2], useMaximalCoordinates=True)
+# this had no effect
+# p.changeDynamics(planeId, -1,
+#                  lateralFriction=1,
+#                  spinningFriction=1,
+#                  rollingFriction=1,
+#                  restitution=0.5)
 
-ballId = p.loadSoftBody("./assets/objects/ball.obj", simFileName="./assets/objects/ball.vtk", basePosition=[0, 0, -1], scale=0.5, mass=4, useNeoHookean=1,
-                        NeoHookeanMu=400, NeoHookeanLambda=600, NeoHookeanDamping=0.001, useSelfCollision=1,
-                        frictionCoeff=.5, collisionMargin=0.001)
-p.setTimeStep(0.001)
-p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
-
-# logId = p.startStateLogging(p.STATE_LOGGING_PROFILE_TIMINGS, "perf.json")
-
-while p.isConnected():
+for _ in range(10000):
     p.stepSimulation()
-    # there can be some artifacts in the visualizer window,
-    # due to reading of deformable vertices in the renderer,
-    # while the simulators updates the same vertices
-    # it can be avoided using
-    # p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING,1)
-    # but then things go slower
-    p.setGravity(0, 0, -10)
-    # sleep(1./240.)
-
-# p.resetSimulation()
-# p.stopStateLogging(logId)
+    time.sleep(1. / 240.)
+p.resetSimulation()
