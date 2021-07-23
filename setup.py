eff72d3ff2e47cd79cf0aaae06e87da835f566d0
llach <lachluca@gmail.com>
2021-07-23 10:18:18 +0200

    install all subpackages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(name='tiago_rl',
       version='0.1',
@@ -6,5 +6,5 @@ setup(name='tiago_rl',
       author='Luca Lach',
       author_email='luca.lach@pal-robotics.com',
       url='https://github.com/llach/tiago_rl',
-      packages=['tiago_rl'],
+      packages=[package for package in find_packages() if package.startswith("tiago_rl")],
      )
