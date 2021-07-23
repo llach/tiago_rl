77c8c2bcf0657c4902c3f107eb1cab3d20fcbf99
llach <lachluca@gmail.com>
2021-07-23 09:47:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tiago_rl',
+      version='0.1',
+      description='TIAGo Reinforcement Learning Environments',
+      author='Luca Lach',
+      author_email='luca.lach@pal-robotics.com',
+      url='https://github.com/llach/tiago_rl',
+      packages=['tiago_rl'],
+     )
